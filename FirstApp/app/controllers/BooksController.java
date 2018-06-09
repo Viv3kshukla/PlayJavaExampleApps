129fa7634a3096601ee6372807ccf92d093b328c
Viv3kshukla <vivekshukla@iiitdmj.ac.in>
2018-06-09 16:06:30 +0530

    added delete method

--- FirstApp/app/controllers/BooksController.java
+++ FirstApp/app/controllers/BooksController.java
@@ -65,7 +65,14 @@ public class BooksController extends Controller{
 	}
 	
 	public Result destroy(Integer id) {
-		return TODO;
+		Book book = Book.findById(id);
+		if(book==null) {
+			return notFound("Book not Found");
+		}
+		
+		Book.remove(book);
+		return redirect(routes.BooksController.index());
+		
 	}
 	
 	
